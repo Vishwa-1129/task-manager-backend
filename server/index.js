8fcb260ea9abd8a2af405f711f76a0e8321acba1
Vishwa-1129 <vishwajamalpur1103@gmail.com>
2025-05-14 13:31:17 +0530

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,20 @@
+
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/task-manager';
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('API is running'));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
